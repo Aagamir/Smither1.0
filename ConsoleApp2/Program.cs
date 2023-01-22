cd607334d25b3da0d5918040442e6ed9358871d1
Mikołaj Marcinkowski <mikimarcinkowski@gmail.com>
2023-01-22 19:12:21 +0100

    Poprawienie języka

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,8 +3,8 @@ bool winner = true;
 string[] board = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", };
 string X = "X";
 string O = "O";
-bool poprawnaLiczba = true;
-void Tabelka (string[] board)
+bool correctNumber = true;
+void Table (string[] board)
 {
     Console.WriteLine("________________     ________________");
     Console.WriteLine($"| {board[1]}  | {board[2]}  | {board[3]}  |     | 1  | 2  | 3  |");
@@ -16,31 +16,31 @@ void Tabelka (string[] board)
 };
 for (int i = 0; i < 9; i++)
 {
-    string tura = counter % 2 == 0 ? $"{X}" : $"{O}";
-    string turaKoncowa = counter % 2 != 0 ? $"{X}" : $"{O}";
+    string turn = counter % 2 == 0 ? $"{X}" : $"{O}";
+    string finalTurn = counter % 2 != 0 ? $"{X}" : $"{O}";
     if (board[1] == board[2] && board[2] == board[3] && board[3] != board[0] || board[4] == board[5] && board[5] == board[6] && board[6] != board[0] || board[7] == board[8] && board[8] == board[9] && board[9] != board[0] ||
         board[1] == board[4] && board[4] == board[7] && board[7] != board[0] || board[2] == board[5] && board[5] == board[8] && board[8] != board[0] || board[3] == board[6] && board[6] == board[9] && board[9] != board[0] ||
         board[1] == board[5] && board[5] == board[9] && board[9] != board[0] || board[3] == board[5] && board[5] == board[7] && board[7] != board[0])
     {
-        Console.WriteLine("Wygrywa gracz < " + turaKoncowa + " >");
+        Console.WriteLine("Wygrywa gracz < " + finalTurn + " >");
         winner = false;
         break;
     }
     int userInput;
     do
     {
-        Tabelka(board);
-        poprawnaLiczba = true;
+        Table(board);
+        correctNumber = true;
         userInput = int.Parse(Console.ReadLine());
         if (userInput > 9 || userInput <= 0 || board[userInput] != " ")
         {
             Console.Clear();
-            poprawnaLiczba = false;
+            correctNumber = false;
             Console.WriteLine("Chujowa wartość, podaj jeszcze raz.");
             
         }
-    } while (!poprawnaLiczba);
-        board[userInput] = tura;
+    } while (!correctNumber);
+        board[userInput] = turn;
         counter++;
      Console.Clear();       
 }
@@ -49,5 +49,4 @@ for (int i = 0; i < 9; i++)
             Console.WriteLine("REMIS");
             break;
         }
-       
-        Tabelka(board);
+        Table(board);
